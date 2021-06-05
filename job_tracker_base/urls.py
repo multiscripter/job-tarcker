a717ba78baca1295401c68e38b16e73b17e454a8
multiscripter <ILL-JAH@yandex.ru>
2021-06-05 14:41:28 +0300

    v8. Renamed 'base' package.

--- job_tracker_base/urls.py
+++ job_tracker_base/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from base import settings
+from job_tracker_base import settings
 from job_tracker import views
 
 urlpatterns = [
